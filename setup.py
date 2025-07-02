f07e7b9b462d5111eff5446e11805cbde8dd1f9d
Sahil Dahal <denenme49@gmail.com>
2025-07-02 22:02:47 +0545

    Refactor project structure: update pyproject.toml, remove requirements.txt, and adjust setup.py for package directory

--- setup.py
+++ setup.py
@@ -12,13 +12,14 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sahilverse/esewa-python", 
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.13',
     install_requires=[
         "requests>=2.25.1", 
     ],
